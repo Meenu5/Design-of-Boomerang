31442406c2b3f1640be6cbf1b99f3dc877df69d3
Meenakshi <ravisankarmeenakshi@gmail.com>
2018-05-03 11:08:03 +0530

    Final commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Trajectory simulation of Boomerang by modelling non-linear equations of motion
+Dependencies
+Numpy, Pandas, Scipy, Matplotlib
--- SimulationCodes/aerocoefficients.py
+++ SimulationCodes/aerocoefficients.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 def doLoadLDCoefficients() :
-    data = np.genfromtxt('Cl.csv', delimiter=',', dtype='float64')
+    data = np.genfromtxt('../data/Cl.csv', delimiter=',', dtype='float64')
     Cl = data[:,1]
     alpha_Cl = data[:,2]
 
-    data = np.genfromtxt('Cd.csv', delimiter=',', dtype='float64')
+    data = np.genfromtxt('../data/Cd.csv', delimiter=',', dtype='float64')
     Cd = data[:,1]
     alpha_Cd = data[:,2]
 
     return Cl, alpha_Cl, Cd, alpha_Cd
 
 def doLoadMomentCoefficients() :
-    data = np.genfromtxt('Cm.csv', delimiter=',', dtype='float64')
+    data = np.genfromtxt('../data/Cm.csv', delimiter=',', dtype='float64')
     Cm = data[:,1]
     alpha_Cm = data[:,2]
 
